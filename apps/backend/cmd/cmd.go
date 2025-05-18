99afc4de61e0865032da6783c1dafa6d9d2c35ad
Савельев Артём Иванович <aisavelev@edu.hse.ru>
2025-05-19 02:03:56 +0300

    changed project struct

--- apps/backend/cmd/cmd.go
+++ apps/backend/cmd/cmd.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/vedsatt/laboratory-works-checker/internal/config"
 	server "github.com/vedsatt/laboratory-works-checker/internal/http-server"
-	tcpclient "github.com/vedsatt/laboratory-works-checker/internal/tcp-client"
 )
 
 func main() {
@@ -11,7 +10,5 @@ func main() {
 
 	server := server.New(cfg.Port)
 
-	go server.Start()
-
-	tcpclient.Connect()
+	server.Start()
 }
