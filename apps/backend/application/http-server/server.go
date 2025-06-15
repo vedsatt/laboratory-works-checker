d5256035d50cf77ca3a26b08e9ffb114129dfa01
vedsatt <retr0artem@mai.ru>
2025-06-15 17:23:53 +0300

    ssl

--- apps/backend/application/http-server/server.go
+++ apps/backend/application/http-server/server.go
@@ -2,7 +2,6 @@
 package server
 
 import (
-	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,50 +28,18 @@ func New(port, httpsPort, certFile, keyFile string) *Server {
 	return s
 }
 
-// Запускает сервер с поддержкой TLS протокола
+// Запускает сервер с поддержкой SSL протокола
+// certFile - путь к SSL-сертификату (например, "cert.pem")
+// keyFile - путь к приватному ключу (например, "key.pem")
 func (s *Server) Start() {
 	submit := http.HandlerFunc(SubmitLabHandler)
 
-	// Мьюх нужен, чтобы обслуживать мидлвееры
 	mux := http.NewServeMux()
-
-	// Хендлер для отправки работ на проверку
 	mux.Handle("/api/v1/submit", logsMiddleware(enableCORS(submit)))
 
-	go func() {
-		log.Printf("Starting HTTP server on port: %v (redirect to HTTPS)", s.port)
-		err := http.ListenAndServe(s.port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "https://"+r.Host+s.httpsPort+r.RequestURI, http.StatusMovedPermanently)
-		}))
-		if err != nil {
-			log.Fatalf("HTTP server failed: %v", err)
-		}
-	}()
-
-	// Настройка TLS конфигурации
-	cfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
-	}
-
-	// Настройка HTTPS сервера
-	server := &http.Server{
-		Addr:         s.httpsPort,
-		Handler:      mux,
-		TLSConfig:    cfg,
-		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
-	}
-
-	log.Printf("Starting HTTPS server on port: %v", s.httpsPort)
-	err := server.ListenAndServeTLS(s.certFile, s.keyFile)
+	log.Printf("Starting SSL (HTTPS) server on port: %v", s.port)
+	err := http.ListenAndServeTLS(s.port, s.certFile, s.keyFile, mux)
 	if err != nil {
-		log.Fatalf("HTTPS server failed: %v", err)
+		log.Fatalf("Failed to start SSL server: %v", err)
 	}
 }
