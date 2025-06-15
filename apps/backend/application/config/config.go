936fc22a7c009ab34fd87e1d72d3d3b63d35c62f
vedsatt <retr0artem@mai.ru>
2025-06-15 17:17:55 +0300

    tls

--- apps/backend/application/config/config.go
+++ apps/backend/application/config/config.go
@@ -7,25 +7,34 @@ import (
 )
 
 var (
-	defPort string = "80"
+	defPort      string = "80"
+	defHttpsPort string = "8443"
 )
 
 // Конфиг сервера
 type Config struct {
-	Port string
+	Port      string
+	HttpsPort string
 }
 
 // Возвращает порт сервера из переменных сред
 func GetConfig() *Config {
 	// Устанавливаем значения по умолчанию сразу
 	c := &Config{
-		Port: defPort,
+		Port:      defPort,
+		HttpsPort: defHttpsPort,
 	}
 
 	if port, ok := os.LookupEnv("PORT"); !ok {
 		log.Printf("Port is unset. Using default value: %v", defPort)
 	} else {
 		c.Port = port
+	}
+
+	if httpsPort, ok := os.LookupEnv("HTTPS_PORT"); !ok {
+		log.Printf("HTTPS port is unset. Using default value: %v", defHttpsPort)
+	} else {
+		c.HttpsPort = httpsPort
 	}
 
 	return c
